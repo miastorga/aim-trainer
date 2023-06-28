3820b24df521cce16d9d9bec842b32df0cdd31d4
Miguel Astorga <mastorga.leiva@gmail.com>
2023-06-27 21:50:25 -0400

    inicio y termino del juego mockup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,19 @@
 import { useState } from 'react'
-import './App.css'
 import { Diana } from './Diana'
 import { useRef } from 'react'
-import { useEffect } from 'react'
+import './App.css'
 
 function App() {
   const [buttonPosition, setButtonPosition] = useState({ top: '50%', left: '50%' })
-  const [remaining, setRemaining] = useState(30)
-
+  const [remaining, setRemaining] = useState(10)
   const dianaContainerRef = useRef()
 
   function handleDianaMovement() {
     setRemaining(remaining => remaining - 1)
-
     const containerReact = dianaContainerRef.current.getBoundingClientRect()
-    console.log(containerReact)
 
-    const maxX = containerReact.width - 100 // <---- ancho del boton
-    const maxY = containerReact.height - 100 // <---- ancho del boton
+    const maxX = containerReact.width - 100 // 100 es el ancho del boton
+    const maxY = containerReact.height - 100 // 100 es el ancho del boton
 
     const newX = Math.floor(Math.random() * maxX)
     const newY = Math.floor(Math.random() * maxY)
@@ -26,7 +22,6 @@ function App() {
       top: newY,
       left: newX,
     }
-    console.log(newButtonPosition)
     setButtonPosition(newButtonPosition)
   }
 
@@ -38,10 +33,16 @@ function App() {
       <main>
         <div className="diana-container">
           <div>
-            <h2>Remaining: {remaining}</h2>
+            {
+              remaining === 0 ? <p>se termino</p> : <h2>Remaining: {remaining}</h2>
+            }
           </div>
           <div style={{ width: '100%', position: 'relative', height: '90%' }} ref={dianaContainerRef}>
-            <Diana handleDianaMovement={handleDianaMovement} buttonPosition={buttonPosition} />
+            {
+              remaining > 0 && (
+                <Diana handleDianaMovement={handleDianaMovement} buttonPosition={buttonPosition} />
+              )
+            }
           </div>
         </div>
       </main>
--- src/Diana.jsx
+++ src/Diana.jsx
@@ -1,14 +1,15 @@
 /* eslint-disable react/prop-types */
+import diana from './assets/diana .svg'
 import './App.css'
 export const Diana = ({ handleDianaMovement, buttonPosition }) => {
 
   return (
-    <button className='diana-btn' onClick={handleDianaMovement}
-      style={{
-        position: 'absolute',
+    <div>
+      <img src={diana} alt="" className='diana-svg' style={{
         top: `${buttonPosition.top}px`,
         left: `${buttonPosition.left}px`,
-      }}
-    >Click</button>
+        userSelect: 'none'
+      }} onClick={handleDianaMovement} draggable="false" onMouseDown="return false" />
+    </div>
   )
 }
